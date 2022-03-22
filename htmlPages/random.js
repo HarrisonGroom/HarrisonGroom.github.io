e43160ebef3e0e55b92bae05afb7740145a092fa
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-03-22 15:04:09 -0600

    updates

--- htmlPages/random.js
+++ htmlPages/random.js
@@ -4,3 +4,12 @@ function change() // no ';' here
     if (elem.value=="hello") elem.value = "goodbye";
     else elem.value = "hello";
 }
+
+
+
+
+//button 3
+function myFunction() {
+    var x = document.getElementById("myInput");
+    document.getElementById("demo").innerHTML = "searching for: " + x.value;
+}
