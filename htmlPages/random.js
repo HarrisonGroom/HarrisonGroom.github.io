fd52f5f346b79a1dcbf43f6931e73cb18eed0efe
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-03-25 13:27:36 -0600

    fixed the random

--- htmlPages/random.js
+++ htmlPages/random.js
@@ -19,37 +19,68 @@ function myFunction() {
 
 
 // random number
-const app = new Vue({
-	el: "#app",
-  data: {
-    min: 1,
-    max: 10,
-    number: 1,
-  },
-  created: function () {
-    this.getRandomNumber()
-  },
-  methods: {
-    loadNum: function () {
-      this.min = 1;
-      this.max = 10;
-      this.getRandomNumber();
-    },
-    getInput: function () {
-      let min = Number(this.min)
-      let max = Number(this.max)
-      if(min > max) {
-        [min, max] = [max, min]
-      }
-      this.min = min
-      this.max = max
-      this.getRandomNumber()
-    },
-    getRandomNumber: function () {
-      this.number = this.generateNumber()
-    },
-    generateNumber: function () {
-      return Math.floor(Math.random()*(this.max-this.min+1)+this.min);
+var randomNr;
+var id_nr_game;
+var check_answer;
+var msg_confirm;
+var counter = 0;
+var counter_msg;
+
+randomNr = Math.floor((Math.random() * 100) + 1);
+
+document.getElementById('id_button').addEventListener('click', function() { 
+  check_nr();
+});
+
+document.getElementById('id_nr_game').addEventListener('keyup', function(event){
+  if (event.which === 13) {
+    check_nr();
+  }
+});
+
+function check_nr() {
+  
+  counter++;
+  
+  id_nr_game = document.getElementById('id_nr_game').value;
+  
+  document.getElementById('id_nr_game').value = '';
+  document.getElementById('id_nr_game').focus();
+  
+  check_answer = document.getElementById('check_answer');
+  counter_msg = document.getElementById('id_counter');
+  
+  counter_msg.innerHTML = 'This is yours: ' + counter + ' attempts.';
+  
+  if (id_nr_game.indexOf('.') != -1 || id_nr_game.indexOf(',') != -1) {
+    check_answer.innerHTML = 'Enter a naturals number to play.';
+  } else if (id_nr_game == '' || isNaN(id_nr_game)) {
+    check_answer.innerHTML = 'Enter a natural number to play.';
+  } else if (id_nr_game < 0 || id_nr_game > 100) {
+    check_answer.innerHTML = 'The number provided is outside the required range (less than 0 or greater than 100).';
+  }   
+  else {
+      
+      if(id_nr_game == randomNr) {
+      msg_confirm = window.confirm('Way to go! You are a winner! You have succeeded in ' + (counter--) + ' total! Do you want to play again?');
+        check_answer.innerHTML = '';
+        if(msg_confirm == true) {
+          randomNr = Math.floor((Math.random() * 100) + 1);
+          counter_msg.innerHTML = '';
+          counter = 0;
+        } else {
+          document.getElementById('game_field').style.display = 'none';
+          check_answer.innerHTML = 'Gratulujemy wygranej. Dziękujemy za wspólną grę.';
+          check_answer.style.color = 'red';
+          counter_msg.innerHTML = 'The correct number was given for: ' + (counter--) + 'total';
+        }
+      
+    } else if(id_nr_game > randomNr) {
+      check_answer.innerHTML = 'The given number is greater than random. try again!';
+    } else {
+      check_answer.innerHTML = 'The given number is less than random. try again!';
     }
   }
-})
+  
+  //alert(randomNr);
+}
