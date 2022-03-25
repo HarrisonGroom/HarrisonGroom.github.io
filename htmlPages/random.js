7905e2ff3319ae49f19f4e32d70a9467c44a6aba
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-03-25 12:16:18 -0600

    update to random

--- htmlPages/random.js
+++ htmlPages/random.js
@@ -13,3 +13,43 @@ function myFunction() {
     var x = document.getElementById("myInput");
     document.getElementById("demo").innerHTML = "searching for: " + x.value;
 }
+
+
+
+
+
+// random number
+const app = new Vue({
+	el: "#app",
+  data: {
+    min: 1,
+    max: 10,
+    number: 1,
+  },
+  created: function () {
+    this.getRandomNumber()
+  },
+  methods: {
+    loadNum: function () {
+      this.min = 1;
+      this.max = 10;
+      this.getRandomNumber();
+    },
+    getInput: function () {
+      let min = Number(this.min)
+      let max = Number(this.max)
+      if(min > max) {
+        [min, max] = [max, min]
+      }
+      this.min = min
+      this.max = max
+      this.getRandomNumber()
+    },
+    getRandomNumber: function () {
+      this.number = this.generateNumber()
+    },
+    generateNumber: function () {
+      return Math.floor(Math.random()*(this.max-this.min+1)+this.min);
+    }
+  }
+})
