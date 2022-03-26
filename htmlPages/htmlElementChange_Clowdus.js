b769ff51b5cee0b519c90b67e0a6d0262bc5164c
SteveClowdus <97994000+SteveClowdus@users.noreply.github.com>
2022-03-26 15:48:55 -0600

    Update htmlElementChange_Clowdus.js

--- htmlPages/htmlElementChange_Clowdus.js
+++ htmlPages/htmlElementChange_Clowdus.js
@@ -1,5 +1,5 @@
 {
-    const resumeConstant = document.getElementById("#elementToChange");
+    const resumeConstant = document.querySelector("#elementToChange");
     resumeConstant.style.color = "red";
     resumeConstant.innerText = "[My resume here.]";
 }
