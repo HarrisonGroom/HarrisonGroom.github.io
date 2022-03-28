dd3e5fe116acb7f423109fe9073886d5f68a91b6
SteveClowdus <97994000+SteveClowdus@users.noreply.github.com>
2022-03-27 22:58:28 -0600

    Update htmlElementChange_Clowdus.js

--- htmlPages/htmlElementChange_Clowdus.js
+++ htmlPages/htmlElementChange_Clowdus.js
@@ -1,5 +1,6 @@
 function changeHtmlElement() {
-    const resumeConstant = document.getElementById("elementToChange").innerHTML;
-    resumeConstant = "[My resume here.]";
-    resumeConstant.color = "red";
-}
+  var currentResume = document.getElementById('elementToChange');
+  if (currentResume.innerHTML.match("<p>[My resume here.]</p>")){
+    currentResume.innerHTML = "<p>[My updated resume here.]</p>";
+      currentResume/*.innerHTML*/.style.color = "red";
+  }
