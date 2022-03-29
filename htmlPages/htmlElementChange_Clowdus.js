5d7b1c52664ab7ff9f21600889f5dce8e302908d
SteveClowdus <97994000+SteveClowdus@users.noreply.github.com>
2022-03-29 12:15:16 -0600

    Update htmlElementChange_Clowdus.js

--- htmlPages/htmlElementChange_Clowdus.js
+++ htmlPages/htmlElementChange_Clowdus.js
@@ -1,8 +1,11 @@
 function changeHtmlElement() {
-  document.getElementById("elementToChange")).style.color = 'red';
-  document.getElementById("elementToChange")).innerHTML = '[My updated resume here.]';
+  document.getElementById("elementToChange").innerHTML = "Hello JavaScript!"
 }
 
+/*function changeHtmlElement() {
+  document.getElementById("elementToChange")).innerHTML = 'Updated!';
+}
+*/
 /*function changeHtmlElement() {
   document.getElementById("elementToChange")).style.color = 'red';
   document.getElementById("elementToChange")).innerHTML = '[My updated resume here.]';
